933e32726ff17cb42512834f64178a0d74e5a7d3
Alanx Will <alanxwill@users.noreply.github.com>
2017-08-29 21:21:27 +0800

    bug fix

--- luoo.py
+++ luoo.py
@@ -1,10 +1,8 @@
-# -*- coding:utf-8 -*-
-
 import requests
 from bs4 import BeautifulSoup
 import os
 from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, APIC
+from mutagen.id3 import ID3, APIC, TPE1, TIT2, TALB
 
 domain = 'http://www.luoo.net/music/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36','Cookie': 'AspxAutoDetectCookieSupport=1',}
@@ -16,19 +14,30 @@ def getSoup(url):
     gs = BeautifulSoup(gr.text,'html.parser')
     return gs
 
-def fileSave(songName,songUrl):
-    if os.path.isfile(songName):
-        pass
-    else:
-        with open(songName,'wb') as file:
-            file.write(requests.get(songUrl,headers = headers).content)
-
 def addAlbumPic(songName, coverName):
     audio = MP3(songName, ID3=ID3)
     id3 = ID3(songName)
     if id3.getall('APIC'):id3.delall('APIC')
     id3.add(APIC(encoding = 0,mime = 'image/jpeg',type = 3,data = open(coverName,'rb').read()))
     id3.save(v2_version = 3)
+
+def addAlbumPicEnhance(songName, coverName, songArtists, songAlbum):
+    audio = MP3(songName, ID3=ID3)
+    audio.add_tags()
+    audio.save()
+    id3 = ID3(songName)
+    id3.add(TPE1(encoding=3,text=songArtists))
+    id3.add(TIT2(encoding=3,text=songName[:-4]))
+    id3.add(TALB(encoding=3,text=songAlbum))
+    id3.add(APIC(encoding = 0,mime = 'image/jpeg',type = 3,data = open(coverName,'rb').read()))
+    id3.save(v2_version = 3)
+
+def fileSave(songName,songUrl):
+    if os.path.isfile(songName):
+        pass
+    else:
+        with open(songName,'wb') as file:
+            file.write(requests.get(songUrl,headers = headers).content)
 
 def folderMaker(foldername):
     if os.path.exists(foldername):
@@ -37,7 +46,6 @@ def folderMaker(foldername):
         os.mkdir(foldername)
     os.chdir(foldername)
 
-folderMaker('luoo')
 while run:
     indexSoup = getSoup(domain)
     for album in indexSoup.select('.meta.rounded.clearfix a'):
@@ -46,7 +54,7 @@ while run:
         albumName = album.text
         folderMaker(albumName)
         for song in albumSoup.select('.track-wrapper.clearfix'):
-            songName = song.select('.trackname')[0].text[4:] + '.mp3'
+            songName = (song.select('.trackname')[0].text[4:] + '.mp3').replace('/','／').replace('\\','＼')
             coverUrl = song.select('.btn-action-share')[0]['data-img']
             songIndex = albumSoup.select('.track-wrapper.clearfix').index(song) + 1
             songUrl = link.format(albumIndex,'%02d'% songIndex)
@@ -57,7 +65,12 @@ while run:
             except:
                 fileSave(songName,songUrl2)
             fileSave(coverName,coverUrl)
-            addAlbumPic(songName, coverName)
+            try:
+                addAlbumPic(songName, coverName)
+            except:
+                songArtists = albumSoup.select('.track-detail')[songIndex - 1].select('p')[1].text[8:]
+                songAlbum = albumSoup.select('.track-detail')[songIndex - 1].select('p')[2].text[7:]
+                addAlbumPicEnhance(songName, coverName, songArtists, songAlbum)
             os.remove(coverName)
         os.chdir(os.path.pardir)
     try:
